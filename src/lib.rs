5e388fd2fbc7128a5639fa8145c795ba62afb2e7
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2024-04-16 11:50:09 -0400

    Add clippy flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#![warn(clippy::pedantic, clippy::nursery, clippy::perf, clippy::style)]
+#![deny(
+    clippy::suspicious,
+    clippy::correctness,
+    clippy::complexity,
+    clippy::missing_const_for_fn,
+)]
+#![forbid(unsafe_code)]
+#![allow(clippy::must_use_candidate)]
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![warn(clippy::pedantic, clippy::nursery, clippy::perf, clippy::style)]
+#![deny(
+    clippy::suspicious,
+    clippy::correctness,
+    clippy::complexity,
+    clippy::missing_const_for_fn,
+)]
+#![forbid(unsafe_code)]
+#![allow(clippy::must_use_candidate)]
+
 fn main() {
     println!("Hello, world!");
 }
